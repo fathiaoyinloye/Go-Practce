35d715ff1852643f750c5184d14fa3ef47b5829f
fathiaoyinloye <fathiaoyinloye20@gmail.com>
2026-02-17 14:23:33 +0100

    Triangle Working

--- chapter-five/printTriangleSidBySide.go
+++ chapter-five/printTriangleSidBySide.go
@@ -4,23 +4,58 @@ import "fmt"
 func printRighAngleTriangleSideBySide(){
 	var firstTriangle = 1;
 	var firstSpaceBegining = 2;
-	var firstSpaceEnding = 12;
+	var secondTriangleBegining = 13
+	var secondTriangleEnding = 22
+	var secondSpaceBegining = 23
+	var secondSpaceEnding = 24
+	var thirdTriangleBegining = 25
+	var thirdTriangleSpaceEnding = 45
+	var fourthTrianglBegining = 46
+
+	
 	
 	for count := 1; count <=10;count++{
 		for counter := 1; counter <= 46; counter++{
-			if count <= firstTriangle && count <=12{
+			if counter <= firstTriangle && counter <=12{
 			fmt.Print("*")
 			}
-			if counter >= firstSpaceBegining && counter >= firstSpaceEnding{
+			if counter >= firstSpaceBegining && counter <= 12{
 				fmt.Print(" ")
 			}
+			if counter >= secondTriangleBegining && counter <= secondTriangleEnding{
+				fmt.Print("*")
+			}
+			if counter >= secondSpaceBegining && counter <= secondSpaceEnding{
+				fmt.Print(" ")
+			}
+			
+			if counter >= thirdTriangleBegining && counter <= 34{
+				fmt.Print("*")
+			}
+			if counter >= 35 && counter <= thirdTriangleSpaceEnding{
+				fmt.Print(" ")
+			}
+			if counter >= fourthTrianglBegining && counter <= 46{
+				fmt.Print("*")
+			}
+			
 
 		}
 		fmt.Println()
-		firstSpaceBegining = firstSpaceBegining + 1
-		
-	
+		firstTriangle = firstTriangle + 1
+		firstSpaceBegining = firstSpaceBegining + 1;
+		secondTriangleEnding = secondTriangleEnding - 1;
+		secondSpaceBegining = secondSpaceBegining - 1;
+		secondSpaceEnding = secondSpaceEnding + 1;
+		thirdTriangleBegining = thirdTriangleBegining +  1
+		thirdTriangleSpaceEnding = thirdTriangleSpaceEnding -  1
+		fourthTrianglBegining = fourthTrianglBegining - 1
+
 	}
 
 
+}
+
+func main(){
+printRighAngleTriangleSideBySide()
 }
